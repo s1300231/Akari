528e588d9aab3f84bb3400b94ca4f2e974e9a6b0
s1300231 <136040486+s1300231@users.noreply.github.com>
2023-06-12 12:45:37 +0900

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -3,20 +3,17 @@
 
   int main(){
     int i,num,heads=0,tails=0;
-    char str[8];
-    printf("誰ですか？");
-    scanf("%s",str);
-    printf("こんにちは、%s\n",str);
+    
 
     printf("Tossing a coin...\n");
     for(i=0;i<3;i++){
       num=rand();
       if(num%2==0){
-        printf("Round %d: Head\n",i+1);
+        printf("Round %d: Heads\n",i+1);
         heads++;
       }
       else{
-        printf("Round %d: Tail\n",i+1);
+        printf("Round %d: Tails\n",i+1);
         tails++;
       }
     }printf("Heads: %d,Tails: %d\n",heads,tails);
